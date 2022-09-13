c67a806bb1334b65b172d638a5fbc861ae20b3ad
piasek <piaskowski3456@gmail.com>
2022-09-13 10:02:15 +0200

    pierwszy

---
+++ app/src/main/java/com/fpp/pierwsza_lekcja/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fpp.pierwsza_lekcja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
